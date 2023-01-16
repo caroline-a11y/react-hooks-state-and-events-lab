c374709934bf6989a7ca318f5e9fa01722792c34
Caroline <caroline.syokau@student.moringaschool.com>
2023-01-16 03:04:41 +0300

    completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
+import React, {useState} from "react";
 import React from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false)
+  const cartClass = inCart ? "in-cart" : ""
+
+  function handleClick(){
+    setInCart(!cart)
+  }
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart": ""}>
+      <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
+      <button onClick={handleAddToCart}className="add">{inCart ? "Remove From" : "Add to"} Cart</button>
       <button className="add">Add to Cart</button>
+    </li>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,23 @@
 import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory ] = useState("All")
+ function handleChange(e){
+    setSelectedCategory(e.target.value)
+ }
+ const filterItems = items.filter((item) => {
+  if(selectedCategory=== "All"){
+    
+    return item.category === selectedCategory
+  }
+ })
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+          <select name="filter">
+          <select  onChange={handleChange} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -14,8 +26,12 @@ function ShoppingList({ items }) {
       </div>
       <ul className="Items">
         {items.map((item) => (
+          {items.map((item)=> (
+            {filterItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
+          ))}
+            
       </ul>
     </div>
   );
